990c7f3ff8824ac7daab7dabb0907a5eaea0a78c
starzhangjh <1134488372@qq.com>
2023-07-28 18:03:44 +0800

    基本配置补充

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,22 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  // 开发环境代理配置，请更换target为后端服务器地址
+  server: {
+    host: true,
+    proxy: {
+      // https://cn.vitejs.dev/config/#server-proxy
+      '/dev-api': {
+        target: 'http://172.24.1.61:7101',
+        changeOrigin: true,
+        rewrite: p => p.replace(/^\/dev-api/, '')
+      }
     }
   },
   build: {
