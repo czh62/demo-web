42f38aa0dbc71c7149ec8c51673a75ac26935102
starzhangjh <1134488372@qq.com>
2023-07-31 17:22:08 +0800

    测试多级路由/优化侧边栏折叠时的样式

--- src/layout/components/SidebarItem.vue
+++ src/layout/components/SidebarItem.vue
@@ -5,7 +5,7 @@
         <el-icon v-if="subItem.meta.icon">
           <component :is="subItem.meta.icon"></component>
         </el-icon>
-        <span>{{ subItem.meta.title }}</span>
+        <span class="is-truncated">{{ subItem.meta.title }}</span>
       </template>
       <sidebar-item :menus="subItem.children" />
     </el-sub-menu>
@@ -14,7 +14,7 @@
         <component :is="subItem.meta.icon"></component>
       </el-icon>
       <template #title>
-        <span>{{ subItem.meta.title }}</span>
+        <span class="is-truncated">{{ subItem.meta.title }}</span>
       </template>
     </el-menu-item>
   </template>
@@ -39,5 +39,3 @@ const handleClickMenu = subItem => {
   router.push(subItem.path)
 }
 </script>
-
-<style lang="scss" scoped></style>
