07b67d79757522c09b29f78bb6257c0e8e6b6386
starzhangjh <1134488372@qq.com>
2023-08-22 17:43:10 +0800

    修复环境变量前缀应为VITE_APP

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,18 +1,24 @@
 import axios from 'axios'
+import { getToken } from './cookie'
 
 axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
 
 // 创建axios实例
 const request = axios.create({
   // axios中请求配置有baseURL选项，表示请求URL公共部分
-  baseURL: import.meta.env.VUE_APP_BASE_API,
+  baseURL: import.meta.env.VITE_APP_BASE_API,
   // 超时
   timeout: 10000
 })
 
 // 请求拦截器
 request.interceptors.request.use(
-  config => config,
+  config => {
+    if (getToken()) {
+      config.headers['Authorization'] = 'Bearer ' + getToken()
+    }
+    return config
+  },
   error => Promise.reject(error)
 )
 
